b5934b5b8ceae4b18c71aa0052bf2a3ae3f6a5af
Serdar AYDIN <serdaraydin1987@gmail.com>
2022-03-19 17:52:07 +0300

    Recipe Project final status

---
+++ src/main/java/com/guru/recipeproject/services/UnitOfMeasureService.java
@@ -0,0 +1,14 @@
+package com.guru.recipeproject.services;
+
+
+import com.guru.recipeproject.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
